d0306c9e4045ea460f4b64d709e9958b04faacc4
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-03-26 21:29:16 +0900

    add eye tracking

--- assets/js/companion.js
+++ assets/js/companion.js
@@ -8,11 +8,35 @@ export class Companion {
         this.height = height;
         this.speed = 5;
         this.range = 200;
+        this.eyePos = 0;
     }
 
     draw() {
+        // draw body
         context.fillStyle = 'black';
-        context.fillRect(this.x, this.y, this.width, this.height);
+        context.fillRect(this.x - this.width / 2, this.y - this.width / 2, this.width, this.height);
+
+        // draw eye socket
+        context.fillStyle = 'white';
+        context.strokeStyle = 'black';
+        context.beginPath();
+        context.arc(this.x - this.width / 4, this.y - this.height / 2, this.width / 4, 0, Math.PI * 2);
+        context.fill();
+        context.stroke();
+        context.beginPath();
+        context.arc(this.x + this.width / 4, this.y - this.height / 2, this.width / 4, 0, Math.PI * 2);
+        context.fill();
+        context.stroke();
+
+        // draw pupil
+        context.fillStyle = 'black';
+        context.beginPath();
+        context.arc(this.x - this.width / 4 + this.eyePos, this.y - this.height / 2, this.width / 12, 0, Math.PI * 2);
+        context.fill();
+        context.beginPath();
+        context.arc(this.x + this.width / 4 + this.eyePos, this.y - this.height / 2, this.width / 12, 0, Math.PI * 2);
+        context.fill();
+
     }
 
     moveLeft() {
@@ -23,9 +47,21 @@ export class Companion {
         this.x += this.speed;
     }
 
+    moveEyeLeft() {
+        this.eyePos -= 1;
+        if (this.eyePos < -this.width / 6) this.eyePos = -this.width / 6
+    }
+
+    moveEyeRight() {
+        this.eyePos += 1;
+        if (this.eyePos > this.width / 6) this.eyePos = this.width / 6
+    }
+
     track(pos) {
         if (this.x > pos * canvas.width + this.range) this.moveLeft();
         else if (this.x < pos * canvas.width - this.range) this.moveRight();
+
+        this.eyePos = (pos - this.x / canvas.width) * this.width / 6;
     }
 
     drawTarget(pos) {
