15fd6302ce3ece34b3aa3d59abc622ee0aa73793
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-03-26 22:19:15 +0900

    add faceDetection object and follow to end of screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,35 @@
 import { video, initCam } from './video.js';
 import { canvas, context, camCanvas, camContext, setCanvasDimensions } from './canvas.js';
 import { Companion } from './companion.js';
+import { faceDetection } from './faceDetection.js';
 
 let companion;
 
 async function init() {
-    model = await blazeface.load();
+    const model = await blazeface.load();
+    faceDetection.addModel(model);
 
     initCam();
     setCanvasDimensions();
-    companion = new Companion(canvas.width / 2, canvas.height / 2, 50, 50);
+    companion = new Companion(canvas.width / 2, canvas.height / 2, 100, 100);
 
     return model
 }
 
 export async function main() {
-  const predictions = await model.estimateFaces(video, false);
+  await faceDetection.estimateFaces(video);
 
   camContext.drawImage(video, 0, 0, camCanvas.width, camCanvas.height)
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (let prediction of predictions) {
-    const start = prediction.topLeft;
-    const end = prediction.bottomRight;
-    const size = [end[0] - start[0], end[1] - start[1]];
+  faceDetection.drawPredictions();
 
-    camContext.fillStyle = 'rgba(256, 0, 0, 0.5)';
-    camContext.fillRect(start[0], start[1], size[0], size[1]);
-    camContext.fillStyle = 'blue';
-    for (let landmark of prediction.landmarks) {
-      camContext.fillRect(landmark[0] - 4, landmark[1] - 4, 9, 9)
-    }
-
-    companion.track(1 - (start[0] + end[0]) / 2 / camCanvas.width)
-    // companion.drawTarget(1 - (start[0] + end[0]) / 2 / camCanvas.width)
-  }
-
+  const pos = faceDetection.getTrackPos()
+  companion.track(pos)
+  // companion.drawTarget(pos)
   companion.draw();
 
   requestAnimationFrame(main)
 }
 
-let model = init();
+init();
---
+++ assets/js/faceDetection.js
@@ -0,0 +1,50 @@
+import { camCanvas, camContext } from './canvas.js';
+
+class FaceDetection {
+    constructor() {
+    }
+
+    addModel(model) {
+        this.model = model;
+        this.predictions = [];
+    }
+
+    async estimateFaces(video) {
+        const predictions = await faceDetection.model.estimateFaces(video, false);
+        const newPredictions = [];
+        for (let prediction of predictions) 
+            if (prediction.probability[0] > 0.9) {
+                newPredictions.push(prediction)
+                prediction.x = prediction.topLeft[0];
+                prediction.y = prediction.topLeft[1];
+                prediction.width = prediction.bottomRight[0] - prediction.topLeft[0];
+                prediction.height = prediction.bottomRight[1] - prediction.topLeft[1];
+                prediction.area = prediction.width * prediction.height;
+            }
+        
+        if (newPredictions.length) this.predictions = newPredictions;
+    }
+
+    drawPredictions() {
+        for (let prediction of this.predictions) {
+            camContext.fillStyle = 'rgba(256, 0, 0, 0.5)';
+            camContext.fillRect(prediction.x, prediction.y, prediction.width, prediction.height);
+        }
+    }
+
+    getDominantFace() {
+        const dominantFace = this.predictions[0];
+        for (let prediction of this.predictions) 
+            if (prediction.area > dominantFace.area)
+                dominantFace = prediction
+        
+        return dominantFace
+    }
+
+    getTrackPos() {
+        const face = this.getDominantFace();
+        return 1 - (face.x + face.width / 2) / camCanvas.width
+    }
+}
+
+export const faceDetection = new FaceDetection()
