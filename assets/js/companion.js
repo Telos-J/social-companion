fe33ed66396a10dfb278b64a6dffdf6660cad116
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-05-14 23:52:27 +0900

    Blinking and idle animation

--- assets/js/companion.js
+++ assets/js/companion.js
@@ -7,6 +7,7 @@ export class Companion {
         this.height = 100;
         this.range = 600;
         this.dom = document.querySelector('#penguin');
+        this.body = this.dom.querySelector('#body');
         this.face = this.dom.querySelector('#face');
         this.belly = this.dom.querySelector('#belly');
         this.leftWing = this.dom.querySelector('#left-wing');
@@ -16,12 +17,14 @@ export class Companion {
         this.lowerBeak = this.dom.querySelector('#lower-beak');
         this.leftFoot = this.dom.querySelector('#left-foot');
         this.rightFoot = this.dom.querySelector('#right-foot');
+        this.eyelidUpper = this.dom.querySelectorAll('.eye-lid-upper');
+        this.eyelidLower = this.dom.querySelectorAll('.eye-lid-lower');
         this.trackLeft = false;
         this.trackRight = false;
-        this.state = 'idle';
     }
 
     init() {
+        this.animateBlink()
     }
 
     get x() {
@@ -88,6 +91,7 @@ export class Companion {
     }
 
     killTweens() {
+        gsap.killTweensOf(this.body)
         gsap.killTweensOf(this.face)
         gsap.killTweensOf(this.belly)
         gsap.killTweensOf(this.leftWing)
@@ -96,14 +100,18 @@ export class Companion {
         gsap.killTweensOf(this.lowerBeak)
         gsap.killTweensOf(this.leftFoot)
         gsap.killTweensOf(this.rightFoot)
+
+        clearInterval(this.interval)
     }
 
     animateWalkLeft() {
         if (this.state === 'walkLeft') return
+        this.state = 'walkLeft'
 
         this.killTweens();
 
         gsap.timeline({ duration: 0.5 })
+            .to(this.body, { y: 0 }, 0)
             .to(this.face, { x: -8, scaleX: 0.95, }, 0)
             .to(this.belly, { x: -3, }, 0)
             .to(this.leftWing, { x: 3, }, 0)
@@ -123,21 +131,21 @@ export class Companion {
             .to(this.rightFoot, 0.25, { x: 23, y: -10 })
             .to(this.rightFoot, 0.25, { x: 0, y: 0 })
 
-        clearInterval(this.interval)
         gsap.to(this.dom, 0.5, { x: '-=46px' })
         this.interval = window.setInterval(() => {
             gsap.to(this.dom, 0.5, { x: '-=46px' })
         }, 500)
 
-        this.state = 'walkLeft'
     }
 
     animateWalkRight() {
         if (this.state === 'walkRight') return
+        this.state = 'walkRight'
 
         this.killTweens();
 
         gsap.timeline({ duration: 0.5 })
+            .to(this.body, { y: 0 }, 0)
             .to(this.face, { x: 8, scaleX: 0.95, }, 0)
             .to(this.belly, { x: 3, }, 0)
             .to(this.leftWing, { x: -3, }, 0)
@@ -157,35 +165,43 @@ export class Companion {
             .to(this.rightFoot, 0.25, { x: 23, y: 0 })
             .to(this.rightFoot, 0.25, { x: 0, y: 0 })
 
-        clearInterval(this.interval)
         gsap.to(this.dom, 0.5, { x: '+=46px' })
         this.interval = window.setInterval(() => {
             gsap.to(this.dom, 0.5, { x: '+=46px' })
         }, 500)
 
-        this.state = 'walkRight'
     }
 
-    animateIdle() {
+    async animateIdle() {
         if (this.state === 'idle') return
+        this.state = 'idle'
 
         this.killTweens();
 
-        gsap.timeline({ duration: 0.5 })
-            .to(this.face, { x: 0, scaleX: 1, }, 0)
-            .to(this.belly, { x: 0, }, 0)
-            .to(this.leftWing, { x: 0, }, 0)
-            .to(this.rightWing, { x: 0, }, 0)
-            .to(this.beak, { x: 0, }, 0)
-            .to(this.lowerBeak, { x: 0, }, 0)
-
-
-        gsap.timeline({ duration: 0.1, clearProps: "all" })
+        gsap.timeline({ duration: 0.1 })
             .to(this.leftFoot, { x: 0, y: 0 }, 0)
             .to(this.rightFoot, { x: 0, y: 0 }, 0)
 
-        clearInterval(this.interval)
+        await gsap.timeline({ duration: 0.5, })
+            .to(this.body, { y: 0, }, 0)
+            .to(this.face, { x: 0, scaleX: 1, }, 0)
+            .to(this.belly, { x: 0, }, 0)
+            .to(this.leftWing, { x: 0, rotate: 0 }, 0)
+            .to(this.rightWing, { x: 0, rotate: 0 }, 0)
+            .to(this.beak, { x: 0, }, 0)
+            .to(this.lowerBeak, { x: 0, }, 0)
+            .then()
 
-        this.state = 'idle'
+        gsap.timeline({ repeat: -1, yoyo: true })
+            .fromTo(this.body, { y: 0 }, { y: 2, duration: 0.5 })
+            .fromTo(this.leftWing, { rotate: 0 }, { rotate: -5, transformOrigin: "top left", duration: 0.5 }, 0)
+            .fromTo(this.rightWing, { rotate: 0 }, { rotate: 5, transformOrigin: "top right", duration: 0.5 }, 0)
+
+    }
+
+    animateBlink() {
+        gsap.timeline({ repeat: -1, yoyo: true })
+            .to(this.eyelidUpper, { y: 15, duration: 0.1, delay: 4 }, 0)
+            .to(this.eyelidLower, { y: -15, duration: 0.1, delay: 4 }, 0)
     }
 }
